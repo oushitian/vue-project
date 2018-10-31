8615320c8fad3b57596a998b70e0a3c8b351ab61
fengdi <Fd88025442>
2018-10-31 16:37:14 +0800

    增加swagger和mybatis插件的自动代码生成

---
+++ vue-server/src/main/java/com/interest/swagger/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.interest.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author xyl
+ * @Create 2018-09-20 12:07
+ * @Desc 写点注释吧
+ **/
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fd.springbootdemo.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo(){
+        return new ApiInfoBuilder()
+                .title("swagger文档")
+                .description("测试")
+                .version("2.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .build();
+    }
+
+}
