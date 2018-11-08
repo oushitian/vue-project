3e141b80b7a16c8b7a66114f3ab8f6afb2283447
fengdi <Fd88025442>
2018-11-08 18:38:27 +0800

    增加商品修改价格的功能

--- vue-server/src/main/java/com/interest/controller/sys/ProductController.java
+++ vue-server/src/main/java/com/interest/controller/sys/ProductController.java
@@ -9,10 +9,7 @@ import com.interest.service.ProductService;
 import com.interest.utils.PageUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,7 +37,7 @@ public class ProductController {
         return productDetailsService.getProductDetailsByProductId(productId);
     }
 
-    @GetMapping("/product/edit")
+    @PutMapping("/product/edit")
     public ProductModel editProduct(@RequestBody ProductModel productModel){
         productService.editProduct(productModel);
         log.info("edit finish..");
