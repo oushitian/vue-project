e130d25d469fca8f8f15f615c5fdee598307dc2f
fengdi <Fd88025442>
2018-11-09 18:15:57 +0800

    netty代码提交

--- vue-server/src/main/java/com/netty/netty01/client/NettyClient.java
+++ vue-server/src/main/java/com/netty/netty01/client/NettyClient.java
@@ -8,6 +8,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.Delimiters;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 import io.netty.util.CharsetUtil;
@@ -33,6 +35,8 @@ public class NettyClient implements Runnable{
                     @Override
                     protected void initChannel(SocketChannel ch) throws Exception {
                          ch.pipeline()
+                         // 添加帧限定符来防⽌止粘包现象
+                         .addLast(new DelimiterBasedFrameDecoder(8192,Delimiters.lineDelimiter()))
                          .addLast("decoder", new StringDecoder(CharsetUtil.UTF_8))
                          .addLast("encoder", new StringEncoder(CharsetUtil.UTF_8))
                          .addLast("handler", new TcpClientHandler());
